7dfc2aa6fd21a691e8446018fdddcb2dc97031d6
Tom Saleeba <tom.saleeba@gmail.com>
2018-11-19 15:22:58 +1030

    fix: make db-sync entrypoint overwrite the crontab

--- db-sync/entrypoint.sh
+++ db-sync/entrypoint.sh
@@ -2,6 +2,6 @@
 # schedule task in cron and start daemon
 : ${CRON_SCHEDULE:?}
 redirectToDockerLogs='> /proc/1/fd/1 2> /proc/1/fd/2'
-echo "$CRON_SCHEDULE sh /run.sh $redirectToDockerLogs" >> /var/spool/cron/crontabs/root
+echo "$CRON_SCHEDULE sh /run.sh $redirectToDockerLogs" > /var/spool/cron/crontabs/root
 crond -l 2 -f
 
