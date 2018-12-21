c493b33cd13a506a357b4df64c3e6dc64da27060
Tom Saleeba <tom.saleeba@gmail.com>
2018-12-21 11:32:31 +1030

    chore: add a comment to help future devs figure out what the obscure bash parameter expansion syntax is

--- db-sync/entrypoint.sh
+++ db-sync/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # schedule task in cron and start daemon
+
+# assert env vars exist with bash parameter expansion (http://wiki.bash-hackers.org/syntax/pe#display_error_if_null_or_unset)
 : ${CRON_SCHEDULE:?}
+
 redirectToDockerLogs='> /proc/1/fd/1 2> /proc/1/fd/2'
 echo "$CRON_SCHEDULE sh /run.sh $redirectToDockerLogs" > /var/spool/cron/crontabs/root
 crond -l 2 -f
--- es-snapshot/entrypoint.sh
+++ es-snapshot/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -e
+
+# assert env vars exist with bash parameter expansion (http://wiki.bash-hackers.org/syntax/pe#display_error_if_null_or_unset)
 : ${ES_HOST:?}
 : ${ES_PORT:?}
 : ${ES_SNAPSHOT_REPO:?}
 : ${BACKUP_PREFIX:?}
 : ${CRON_SCHEDULE:?}
+
 redirectToDockerLogs='> /proc/1/fd/1 2> /proc/1/fd/2'
 echo "$CRON_SCHEDULE sh /run.sh $redirectToDockerLogs" > /var/spool/cron/crontabs/root
 crond -l 2 -f
