ae67b1299ad76ac9baec0217bd93b067b9fe00cd
Tom Saleeba <tom.saleeba@gmail.com>
2019-03-20 09:12:45 +1030

    refactor: simplify the cron command for es-sync container as logging just work, tidy up log output from cron job

--- es-snapshot/run.sh
+++ es-snapshot/run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+# hits a URL on ElasticSearch to trigger a snapshot.
+set -euxo pipefail
 uniqueFragment=`date +%Y%m%d_%H%M`
 url=http://${ES_HOST}:${ES_PORT}/_snapshot/${ES_SNAPSHOT_REPO}/${BACKUP_PREFIX}.${uniqueFragment}?wait_for_completion=true
-echo "${uniqueFragment} performing ES snapshot"
+echo "$(date) triggering ES snapshot"
 curl -X PUT $url
+echo -e "\n"
 
